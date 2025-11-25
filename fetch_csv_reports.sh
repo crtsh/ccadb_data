42c3993129f486bac0177db20ed1e0a81e282399
Rob Stradling <rob@sectigo.com>
2025-11-25 15:27:05 +0000

    Update script to fetch 'AllCertificateRecordsCSVFormatv4' instead of v2.

--- fetch_csv_reports.sh
+++ fetch_csv_reports.sh
@@ -4,7 +4,7 @@ CURDIR=`pwd`
 TMPDIR=`mktemp -d`
 cd $TMPDIR
 
-wget -nv https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormatv2
+wget -nv https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormatv4
 
 for i in $( seq 1994 `date +%Y` ); do
   wget -nv -O AllCertificatePEMsCSVFormat_NotBeforeYear_$i https://ccadb.my.salesforce-sites.com/ccadb/AllCertificatePEMsCSVFormat?NotBeforeYear=$i
@@ -12,6 +12,6 @@ done
 
 cd $CURDIR
 mkdir -p data
-mv $TMPDIR/AllCertificateRecordsCSVFormatv2 $CURDIR/data
+mv $TMPDIR/AllCertificateRecordsCSVFormatv4 $CURDIR/data
 mv $TMPDIR/* $CURDIR/cmd/ski_spki/data
 rmdir $TMPDIR

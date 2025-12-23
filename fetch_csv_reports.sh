bfdf065a17bdbd7404d805618cca2fe15a84a521
Rob Stradling <rob@sectigo.com>
2025-12-23 20:04:28 +0000

    Sort CSV files sourced from the CCADB.

--- fetch_csv_reports.sh
+++ fetch_csv_reports.sh
@@ -5,9 +5,13 @@ TMPDIR=`mktemp -d`
 cd $TMPDIR
 
 wget -nv https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormatv4
+csvsort AllCertificateRecordsCSVFormatv4 > AllCertificateRecordsCSVFormatv4.sorted
+mv AllCertificateRecordsCSVFormatv4.sorted AllCertificateRecordsCSVFormatv4
 
 for i in $( seq 1994 `date +%Y` ); do
   wget -nv -O AllCertificatePEMsCSVFormat_NotBeforeYear_$i https://ccadb.my.salesforce-sites.com/ccadb/AllCertificatePEMsCSVFormat?NotBeforeYear=$i
+  csvsort AllCertificatePEMsCSVFormat_NotBeforeYear_$i > AllCertificatePEMsCSVFormat_NotBeforeYear_$i.sorted
+  mv AllCertificatePEMsCSVFormat_NotBeforeYear_$i.sorted AllCertificatePEMsCSVFormat_NotBeforeYear_$i
 done
 
 cd $CURDIR
